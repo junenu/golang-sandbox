35cc3771d29ec2547ee881536e232999973d8518
junenu <junenu41@gmail.com>
2024-03-11 09:46:11 +0900

    Update main.go

--- ipaddress/main.go
+++ ipaddress/main.go
@@ -4,17 +4,30 @@ import (
 	"fmt"
 	"regexp"
 	"net"
+	"os"
 )
 
 func main() {
-	text := "このテキストにはIPv4アドレスが含まれています。例えば、192.168.0.1/24や10.0.0.1/8などです。"
 
 	// IPv4アドレスを抽出する正規表現パターン
 	pattern := `\b(?:\d{1,3}\.){3}\d{1,3}(?:\/\d{1,2})?\b`
 
-	// 正規表現パターンにマッチするIPv4アドレスを取得
+	// sample/test.confの内容を読み込む
+	file, err := os.Open("sample/test.conf")
+	if err != nil {
+		fmt.Println("Do Not Open")
+	}
+	defer file.Close()
+
+	// ファイルの内容を取得
+	buf := make([]byte, 1024)
+	file.Read(buf)
+	file.Close()
+	fileStr := string(buf)
+	//fmt.Println(fileStr)
+
 	regex := regexp.MustCompile(pattern)
-	ipAddresses := regex.FindAllString(text, -1)
+	ipAddresses := regex.FindAllString(fileStr, -1)
 
 	// パースしたIPv4アドレスを表示
 	for _, ipAddress := range ipAddresses {
